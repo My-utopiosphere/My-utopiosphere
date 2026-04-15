d61686c071ea4a3f08906495e9a837ab59e04aed
My-utopiosphere <leeyeonsoo2006@gmail.com>
2026-04-16 08:06:35 +0900

    Update 무제.11tydata.js

--- src/site/notes/무제/무제.11tydata.js
+++ src/site/notes/무제/무제.11tydata.js
@@ -2,6 +2,7 @@ module.exports = {
   eleventyComputed: {
     permalink: data => {
       if (data['dg-home']) return '/';
+      if (data.permalink && data.permalink !== '///') return data.permalink;
       return `/notes/무제/${data.page.fileSlug}/`;
     }
   }
